b05291fb25ee194cfe01b6f6b29e553b853c6194
Mohammad Amin Rahimi <marcoding78@gmail.com>
2022-05-27 00:24:19 +0430

    add first version of kivy program

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+from kivy.app import App
+from kivy.uix.button import Button
+
+class Test01App(App):
+    def build(self):
+        return Button(text='Click Me!')
+
+
+
 def main():
-    print("hello")
+    Test01App().run()
 
 
 if __name__ == "__main__":
